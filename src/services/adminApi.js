60df0c4f7d3b671f4afc0fdbba2cdbb19e4cee8d
bante27 <mitikubantalem@gmail.com>
2026-08-21 12:59:49 -0900

    update url

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const ADMIN_API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const ADMIN_API = axios.create({ baseURL: API_BASE_URL });
 
 ADMIN_API.interceptors.request.use((config) => {
   try {
