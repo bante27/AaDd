8eb9393e47b99ab1e13815860768605d34541455
bante27 <mitikubantalem@gmail.com>
2026-08-24 08:42:28 -0900

    update conversation api

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://mrhaile-lms-hub.onrender.com/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 const ADMIN_API = axios.create({ baseURL: API_BASE_URL });
 
 ADMIN_API.interceptors.request.use((config) => {
